542f5b6601158eca463f017de9d6d1847a8cf0d1
Rushabh <rushabhsjain40@gmail.com>
2024-09-27 22:18:17 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -34,14 +34,14 @@ export default class App extends Component {
         onLoaderFinished={() => this.setProgress(0)}
       />
         <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress}  key="home" pageSize={8} country='in' category='general'/></Route>
-          <Route exact path="/business"><News setProgress={this.setProgress}  key="business" pageSize={8} country='in' category='business'/></Route>
-          <Route exact path="/entertainment"><News setProgress={this.setProgress}  key="entertainment" pageSize={8} country='in' category='entertainment'/></Route>
-          <Route exact path="/general"><News setProgress={this.setProgress}  key="general" pageSize={8} country='in' category='general'/></Route>
-          <Route exact path="/health"><News setProgress={this.setProgress}  key="health" pageSize={8} country='in' category='health'/></Route>
-          <Route exact path="/science"><News setProgress={this.setProgress}  key="science" pageSize={8} country='in' category='science'/></Route>
-          <Route exact path="/sports"><News setProgress={this.setProgress}  key="sports" pageSize={8} country='in' category='sports'/></Route>
-          <Route exact path="/technology"><News setProgress={this.setProgress}  key="technology" pageSize={8} country='in' category='technology'/></Route>
+          <Route exact path="/"><News setProgress={this.setProgress}  key="home" pageSize={8} country='us' category='general'/></Route>
+          <Route exact path="/business"><News setProgress={this.setProgress}  key="business" pageSize={8} country='us' category='business'/></Route>
+          <Route exact path="/entertainment"><News setProgress={this.setProgress}  key="entertainment" pageSize={8} country='us' category='entertainment'/></Route>
+          <Route exact path="/general"><News setProgress={this.setProgress}  key="general" pageSize={8} country='us' category='general'/></Route>
+          <Route exact path="/health"><News setProgress={this.setProgress}  key="health" pageSize={8} country='us' category='health'/></Route>
+          <Route exact path="/science"><News setProgress={this.setProgress}  key="science" pageSize={8} country='us' category='science'/></Route>
+          <Route exact path="/sports"><News setProgress={this.setProgress}  key="sports" pageSize={8} country='us' category='sports'/></Route>
+          <Route exact path="/technology"><News setProgress={this.setProgress}  key="technology" pageSize={8} country='us' category='technology'/></Route>
         </Switch>
         </Router>
       </div>
